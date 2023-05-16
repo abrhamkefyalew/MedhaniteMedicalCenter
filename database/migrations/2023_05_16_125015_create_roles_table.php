3a2517403d89ad98f77797b60b7d673a5a0306e8
abrhamkefyalew <abrekefe22@gmail.com>
2023-05-16 15:54:22 +0300

    ROLE  Model,migration,Controller,seeder,Request,Policy Created & migration filled

---
+++ database/migrations/2023_05_16_125015_create_roles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id()->from(10000);
+            
+            $table->string('title')->nullable()->unique();
+            $table->boolean('is_system_created')->default(false);
+            
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
